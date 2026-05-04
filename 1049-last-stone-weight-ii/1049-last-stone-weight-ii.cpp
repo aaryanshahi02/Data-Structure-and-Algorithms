69d0c8c2a2c55818db0174b5a5ee5bdceab89231
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-05-04 12:06:03 +0530

    Time: 0 ms (100%), Space: 10.6 MB (76.96%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for (int x : stones) sum += x;
+
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+
+        for (int stone : stones) {
+            for (int j = target; j >= stone; j--) {
+                
+                dp[j] = dp[j] || dp[j - stone];
+            }
+        }
+
+        for (int j = target; j >= 0; j--) {
+            if (dp[j]) {
+                return sum - 2 * j;
+            }
+        }
+
+        return 0;
+    }
+};
