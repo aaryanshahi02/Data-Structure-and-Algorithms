7df8ed55f87f643b1f77178298eff817424b9343
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-05-12 15:18:43 +0530

    Time: 62 ms (57.01%), Space: 14.4 MB (42.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& arr) {
-        vector<vector<int>> dp(arr.size() + 1, vector<int> (arr.size() + 1, -1));
-        int n = arr.size();
-        for(int i=0; i<=n; i++){
-            dp[n][i] = 0;
-        }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        
 
-        for(int i = n-1; i>=0; i--){
-            for(int j=-1; j<=i-1; j++){
-                int notTake = dp[i+1][j+1];
+        for(int i=1; i<nums.size(); i++){
+            for(int j=i-1; j>=0; j--){
 
-                int take = 0;
-                if(j == -1 || arr[i] > arr[j]){
-                    take = 1 + dp[i+1][i+1];
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], 1 + dp[j]);
                 }
-                dp[i][j+1] = max(take, notTake);
+                
             }
         }
-        // return solve(arr, 0, -1, dp);
-        return dp[0][0];
+        int maxi = INT_MIN;
+        for(int x : dp){
+            
+                maxi = max(maxi, x);
+        }
+        return maxi;
     }
-
-    // int solve(vector<int> &arr, int i, int j, vector<vector<int>> &dp){
-    //     int n = arr.size();
-    //     if(i == n) return 0;
-
-    //     if(dp[i][j+1] != -1) return dp[i][j+1];
-
-    //     int notTake = solve(arr, i+1, j, dp);
-    //     int take = 0;
-    //     if(j == -1 || arr[i] > arr[j]){
-    //         take = 1 + solve(arr, i+1, i, dp);
-    //     }
-    //     return dp[i][j+1] = max(notTake, take);
-    // }
 };
