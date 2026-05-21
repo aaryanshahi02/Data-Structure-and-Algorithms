e04ab14e69128d40097526485afe2c96321c3d36
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-05-21 16:36:47 +0530

    Time: 3 ms (90.7%), Space: 16.7 MB (40.59%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -4,27 +4,24 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
-        return solve(nums1, nums2, 0, 0, dp);
-    }
 
-    int solve(vector<int>& nums1, vector<int>& nums2, int i, int j, vector<vector<int>> &dp){
-        int n = nums1.size();
-        int m = nums2.size();
-
-        if(i == n || j == m) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int eq = 0;
-        int noteq = 0;
-        if(nums1[i] == nums2[j]){
-            eq = 1 + solve(nums1, nums2, i+1, j+1, dp);
-        }else{
-            noteq = max(solve(nums1, nums2, i+1, j, dp), solve(nums1, nums2, i, j+1, dp));
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                dp[n][j] = 0;
+                dp[i][m] = 0;
+            }
         }
 
-        return dp[i][j] = max(eq, noteq);
+        for(int i = n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                
+                if(nums1[i] == nums2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }else{
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
-
-
