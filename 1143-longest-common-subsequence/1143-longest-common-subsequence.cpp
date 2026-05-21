34b0f53b31a3aa2fcf41819e1b12a0859cc30d06
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-05-21 16:16:48 +0530

    Time: 43 ms (21.32%), Space: 27.8 MB (5.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,42 +4,26 @@ public:
         int n = text1.length();
         int m = text2.length();
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=m; j++){
-                dp[n][j] = 0;
-                dp[i][m] = 0;
-            }
-        }
-
-        for(int i = n-1; i>=0; i--){
-            for(int j = m-1; j>=0; j--){
-                if(text1[i] == text2[j]){
-                    dp[i][j] = 1 + dp[i+1][j+1];
-                }else{
-                dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
-                }
-            }
-        }
-
-        return dp[0][0];
-        // return solve(text1, text2, 0, 0, dp);
+        return solve(text1, text2, 0, 0, dp);
     }
 
-    // int solve(string text1, string text2, int i, int j, vector<vector<int>> &dp){
-    //     int n = text1.length();
-    //     int m = text2.length();
+    int solve(string &text1, string &text2, int i, int j, vector<vector<int>> &dp){
+        int n = text1.length();
+        int m = text2.length();
 
-    //     if(i >= n || j >= m) return 0;
+        if(i == n || j == m) return 0;
 
-    //     if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
 
-    //     int eq = 0;
-    //     if(text1[i] == text2[j]){
-    //         eq = 1 + solve(text1, text2, i + 1, j + 1, dp);
-    //     }
-    //     int noteq = max(solve(text1, text2, i+1, j, dp), solve(text1, text2, i, j+1, dp));
-
-    //     return dp[i][j] = max(eq, noteq);
-    // }
+        int eq = 0;
+        
+        int noteq = 0;
+        if(text1[i] == text2[j]){
+            eq = 1 + solve(text1, text2, i + 1, j + 1, dp);
+        }else{
+        noteq = max(solve(text1, text2, i+1, j, dp), solve(text1, text2, i, j+1, dp));
+        }
+        return dp[i][j] = max(eq, noteq);
+    }
 
 };
