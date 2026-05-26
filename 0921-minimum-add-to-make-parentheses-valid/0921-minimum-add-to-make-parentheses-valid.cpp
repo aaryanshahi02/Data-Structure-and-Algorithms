2e7f53a35565f08b2cb247bc438ebd7708f72d0b
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-05-26 14:18:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (10.51%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<int> st;
+        int count = 0;
+
+        for(auto it : s){
+            if(it == '('){
+                st.push(it);
+            }
+
+            else{
+
+                if(!st.empty() && st.top() == '(') st.pop();
+                
+                else{
+                    count++;
+                }
+                
+            }
+
+        }
+        return count + st.size();
+    }
+};
