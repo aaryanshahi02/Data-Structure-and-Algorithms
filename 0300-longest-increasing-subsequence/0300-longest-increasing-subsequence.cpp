79f59850a94b2e86cc3ebe7ac30338ef3068df9e
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-07-03 08:54:14 +0530

    Time: 251 ms (34.48%), Space: 296.3 MB (25.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
-        int n = nums.size();
-        if(i == n) return 0;
-
-        if(dp[i][j+1] != -1) return dp[i][j+1];
-
-        int take = 0;
-        if(j == -1 || nums[i] > nums[j]){
-            take = 1 + solve(i+1, i, nums, dp);
+    int lengthOfLIS(vector<int>& arr) {
+        vector<vector<int>> dp(arr.size() + 1, vector<int> (arr.size() + 1, -1));
+        int n = arr.size();
+        for(int i=0; i<=n; i++){
+            dp[n][i] = 0;
         }
 
-        int notTake = solve(i+1, j, nums, dp);
+        for(int i = n-1; i>=0; i--){
+            for(int j=-1; j<=i-1; j++){
+                int notTake = dp[i+1][j+1];
 
-        return dp[i][j+1] = max(take, notTake);
+                int take = 0;
+                if(j == -1 || arr[i] > arr[j]){
+                    take = 1 + dp[i+1][i+1];
+                }
+                dp[i][j+1] = max(take, notTake);
+            }
+        }
+        // return solve(arr, 0, -1, dp);
+        return dp[0][0];
     }
 
+    // int solve(vector<int> &arr, int i, int j, vector<vector<int>> &dp){
+    //     int n = arr.size();
+    //     if(i == n) return 0;
 
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return solve(0, -1, nums, dp);
-    }
+    //     if(dp[i][j+1] != -1) return dp[i][j+1];
+
+    //     int notTake = solve(arr, i+1, j, dp);
+    //     int take = 0;
+    //     if(j == -1 || arr[i] > arr[j]){
+    //         take = 1 + solve(arr, i+1, i, dp);
+    //     }
+    //     return dp[i][j+1] = max(notTake, take);
+    // }
 };
