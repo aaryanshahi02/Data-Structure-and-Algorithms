cdcb8e0a0ccbc9c993b03dab03286f730ed77284
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-08-06 20:19:24 +0530

    Time: 25 ms (71.7%), Space: 24.4 MB (13.1%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-
     int rowDir[4] = {-1, 1, 0, 0};
     int colDir[4] = {0, 0, -1, 1};
 
     bool isValid(int row, int col, int n, int m){
-        return (row >= 0 && row<n && col>=0 && col<m);
+        return(row >= 0 && row < n && col >= 0 && col < m);
     }
 
-    void bfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int row, int col, int n, int m){
+    void bfs(vector<vector<char>>& grid, vector<vector<int>> &vis, int row, int col, int n, int m){
         queue<pair<int, int>> q;
 
         q.push({row, col});
@@ -19,21 +18,17 @@ public:
             q.pop();
 
             for(int i=0; i<4; i++){
-                int newRow = currRow+rowDir[i];
-                int newCol = currCol+colDir[i];
+                int newRow = currRow + rowDir[i];
+                int newCol = currCol + colDir[i];
 
                 if(isValid(newRow, newCol, n, m) && grid[newRow][newCol] == '1' && vis[newRow][newCol] == 0){
                     vis[newRow][newCol] = 1;
                     q.push({newRow, newCol});
                 }
             }
+
         }
     }
-
-
-
-
-
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
@@ -46,6 +41,7 @@ public:
                 if(grid[i][j] == '1' && vis[i][j] == 0){
                     bfs(grid, vis, i, j, n, m);
                     count++;
+
                 }
             }
         }
