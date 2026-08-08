7012f7c222af4134917edf6946653e677a21727c
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-08-08 11:46:49 +0530

    Time: 64 ms (8.53%), Space: 49.9 MB (19.16%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int solve(int i, vector<int> &coins, int amount, vector<vector<int>> &dp){
-        int n = coins.size();
-
         if(amount == 0) return 0;
         if(i == coins.size()) return 1e9;
 
@@ -10,19 +8,20 @@ public:
 
         int take = 1e9;
         if(coins[i] <= amount){
-            take = 1 + solve(i, coins, amount-coins[i], dp);
+            take = 1 + solve(i, coins, amount - coins[i], dp);
         }
-        int skip = solve(i+1, coins, amount, dp);
 
-        return dp[i][amount] = min(take, skip);
+        int notTake = solve(i+1, coins, amount, dp);
+
+        return dp[i][amount] = min(take, notTake);
 
     }
-
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = solve(0, coins, amount, dp);
-        return(ans >= 1e9) ? -1 : ans;
+        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+
+        int result = solve(0, coins, amount, dp);
+
+        return (result >= 1e9) ? -1 : result;
     }
 };
