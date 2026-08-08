d8b9d7128f5bd0dacaa714026b51ea68a16caab0
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-08-08 17:58:08 +0530

    Time: 203 ms (8.64%), Space: 22.6 MB (10.1%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return solve(0, nums, dp);
+    }
+
     int solve(int i, vector<int> &nums, vector<int> &dp){
-        int n = nums.size();
-        int ans = 1e9;
-
-        if(i >= n-1) return 0;
-
+        if(i >= nums.size()-1) return 0;
         if(dp[i] != -1) return dp[i];
+        int ans = 1e9;
 
         for(int j=1; j<=nums[i]; j++){
             ans = min(ans, 1+solve(i+j, nums, dp));
         }
         return dp[i] = ans;
-    }
-    int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return solve(0, nums, dp);
     }
 };
