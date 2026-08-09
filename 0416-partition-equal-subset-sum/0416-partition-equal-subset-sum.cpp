40d8bef7eb9d31c9453dd282f5e492b3fa917c3e
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-08-10 00:09:16 +0530

    Time: 167 ms (25.09%), Space: 110.6 MB (23.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int total = 0;
-        for(int x : nums) total+=x;
-
-        if(total % 2 != 0) return false;
-
-        int target = total / 2;
-        vector<vector<bool>> dp(nums.size() + 1, vector<bool> (target+1, false));
-        // vector<vector<bool>> vis(nums.size() + 1, vector<bool> (target+1, false));
-
-        for(int i=0; i<=nums.size(); i++){
-            dp[i][0] = true;
-        }
         int n = nums.size();
 
-        for(int i = n-1; i>=0; i--){
-            for(int j=1; j<=target; j++){
-                bool notTake = dp[i+1][j];
-
-                bool take = false;
-                if(nums[i] <= j){
-                    take = dp[i+1][j-nums[i]];
-                }
-                dp[i][j] = take || notTake;
-            }
+        int total = 0;
+        for(int num:nums){
+            total += num;
         }
-        return dp[0][target];
+        if (total % 2 != 0) return false;
+        int target = total/2;
 
-
-        // return solve(target, 0, nums, dp, vis);
+        vector<vector<int>> dp(n, vector<int> (target+1, -1));
+        return solve(nums, target, 0, dp);
     }
 
-    // bool solve(int target, int i, vector<int> &nums, vector<vector<bool>> &dp, vector<vector<bool>> &vis){
-    //     if(target == 0) return true;
-    //     if(i == nums.size()) return false;
+    bool solve(vector<int> &nums, int target, int i, vector<vector<int>> &dp){
+        if(target == 0) return true;
+        if(i == nums.size()) return false;
 
-    //     if(vis[i][target]) return dp[i][target];
+        if(dp[i][target] != -1) return dp[i][target];
 
-    //     bool notTake = solve(target, i+1, nums, dp, vis);
+        bool take = false;
+        if(nums[i] <= target){
+            take = solve(nums, target-nums[i], i+1, dp);
+        }
 
-    //     bool take = false;
-    //     if(nums[i] <= target){
-    //         take = solve(target-nums[i], i+1, nums, dp, vis);
-    //     }
-    //     vis[i][target] = true;
-    //     return dp[i][target] = take || notTake;
-    // }
+        bool notTake = solve(nums, target, i+1, dp);
+
+        return dp[i][target] = take || notTake;
+    }
 };
