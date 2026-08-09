6698e31e0ed5cdda657adc99c1806a3cae12421b
Aaryan Shahi <aaryanshahi256@gmail.com>
2026-08-09 18:45:09 +0530

    Time: 0 ms (100%), Space: 15.2 MB (19.44%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-
-    int rowDir[4] = {1, -1, 0, 0};
-    int colDir[4] = {0, 0, 1, -1};
+    int rowDir[4] = {-1, 1, 0, 0};
+    int colDir[4] = {0, 0, -1, 1};
 
     bool isValid(int row, int col, int n, int m){
-        return (row >= 0 && row < n && col >= 0 && col < m);
+        return(row >= 0 && row < n && col >= 0 && col < m);
     }
 
-    void bfs(vector<vector<char>>& board,
-             vector<vector<int>>& vis,
-             int row, int col,
-             int n, int m){
-
-        queue<pair<int,int>> q;
+    void bfs(vector<vector<char>> &board,vector<vector<int>> &vis, int row, int col, int n, int m){
+        queue<pair<int, int>> q;
         q.push({row, col});
         vis[row][col] = 1;
         board[row][col] = 'A';
 
         while(!q.empty()){
-            auto p = q.front();
+            auto[currRow, currCol] = q.front();
             q.pop();
 
-            int currRow = p.first;
-            int currCol = p.second;
-
-            for(int i = 0; i < 4; i++){
+            for(int i=0; i<4; i++){
                 int newRow = currRow + rowDir[i];
                 int newCol = currCol + colDir[i];
 
-                if(isValid(newRow, newCol, n, m) &&
-                   board[newRow][newCol] == 'O' &&
-                   vis[newRow][newCol] == 0){
-
+                if(isValid(newRow, newCol, n, m) && board[newRow][newCol] == 'O' && vis[newRow][newCol] == 0){
                     vis[newRow][newCol] = 1;
                     q.push({newRow, newCol});
                     board[newRow][newCol] = 'A';
@@ -41,41 +30,39 @@ public:
         }
     }
 
-    void solve(vector<vector<char>>& board) {
 
+
+    void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
 
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        for(int j = 0; j < m; j++){
-            if(board[0][j] == 'O' && vis[0][j] == 0)
+        for(int j=0; j<m; j++){
+            if(board[0][j] == 'O' && vis[0][j] == 0){
                 bfs(board, vis, 0, j, n, m);
-
-            if(board[n-1][j] == 'O' && vis[n-1][j] == 0)
+            }
+            if(board[n-1][j] == 'O' && vis[n-1][j] == 0){
                 bfs(board, vis, n-1, j, n, m);
+            }
         }
 
-        for(int i = 0; i < n; i++){
-            if(board[i][0] == 'O' && vis[i][0] == 0)
+        for(int i=0; i<n; i++){
+            if(board[i][0] == 'O' && vis[i][0] == 0){
                 bfs(board, vis, i, 0, n, m);
-
-            if(board[i][m-1] == 'O' && vis[i][m-1] == 0)
+            }
+            if(board[i][m-1] == 'O' && vis[i][m-1] == 0){
                 bfs(board, vis, i, m-1, n, m);
+            }
         }
 
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                // if(board[i][j] == 'O' && vis[i][j] == 0)
-                //     board[i][j] = 'X';
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(board[i][j] == 'O')
                     board[i][j] = 'X';
-                
-                else if(board[i][j] == 'A')
-                board[i][j] = 'O';
 
-                
+                else if(board[i][j] == 'A')
+                    board[i][j] = 'O';
             }
         }
     }
